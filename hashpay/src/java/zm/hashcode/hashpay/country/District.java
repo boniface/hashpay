2f7707e3052c59033febfd17d2de21ae93938172
peterphillip99@gmail.com <peterphillip99@gmail.com@f78314b2-38be-a658-9164-56e6b81b6566>
2011-08-13 22:19:27 +0000

    1. added package country. 2. removed sales reference in the hashpayPU (caused project to fail building as it was not finding the path referenced)

---
+++ hashpay/src/java/zm/hashcode/hashpay/country/District.java
@@ -0,0 +1,15 @@
+/*Developer: Peter Phillip - Cubetec 
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zm.hashcode.hashpay.country;
+
+/**
+ *
+ * @author Peter Phillip
+ */
+public class District {
+        private String name;
+    private String code;
+}
