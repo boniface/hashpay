32b53027e4996826d7c8207e6cea971972a715d8
sikwata.thozamile@gmail.com <sikwata.thozamile@gmail.com@f78314b2-38be-a658-9164-56e6b81b6566>
2011-08-19 19:37:41 +0000

    Added  interface under services named ProductService and its Implementation class named ProductServiceJPAImpl.

---
+++ hashpay/src/java/zm/hashcode/hashpay/services/jpa/Impl/ProductServiceJPAImpl.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zm.hashcode.hashpay.services.jpa.Impl;
+
+import java.util.List;
+import zm.hashcode.hashpay.model.market.Product;
+import zm.hashcode.hashpay.services.jpa.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import zm.hashcode.hashpay.repository.jpa.ProductDAO;
+
+/**
+ *
+ * @author 209042869
+ */
+@Repository("productService")
+@Transactional
+public class ProductServiceJPAImpl implements ProductService{
+  
+    @Autowired
+    private ProductDAO productDAO;
+    
+    
+    @Override
+    public Product find(Long id) {
+         return productDAO.find(id);
+    }
+
+    @Override
+    public void persist(Product entity) {
+          productDAO.persist(entity);
+    }
+
+    @Override
+    public void merge(Product entity) {
+       productDAO.merge(entity);
+    }
+
+    @Override
+    public void remove(Product entity) {
+        productDAO.remove(entity);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return productDAO.findAll();
+    }
+
+    @Override
+    public List<Product> findInRange(int firstResult, int maxResults) {
+       return productDAO.findInRange(firstResult, maxResults);
+    }
+
+    @Override
+    public long count() {
+         return productDAO.count();
+    }
+
+    @Override
+    public Product getByPropertyName(String name, String value) {
+        return productDAO.getByPropertyName(name, value);
+    }
+
+    @Override
+    public List<Product> getEntitiesByProperName(String name, String value) {
+        return productDAO.getEntitiesByProperName(name, value);
+    }
+    
+}
---
+++ hashpay/src/java/zm/hashcode/hashpay/services/jpa/ProductService.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zm.hashcode.hashpay.services.jpa;
+import zm.hashcode.hashpay.model.market.Product;
+import zm.hashcode.hashpay.services.Service;
+
+/**
+ *
+ * @author 209042869
+ */
+public interface ProductService extends Service<Product, Long>{
+    
+}
