da77d5d31381677366a93159e8a023a605fa861f
bonganim911@gmail.com <bonganim911@gmail.com@f78314b2-38be-a658-9164-56e6b81b6566>
2011-08-19 18:38:35 +0000

    Added VoucherDAO in a repository

---
+++ hashpay/src/java/zm/hashcode/hashpay/repository/jpa/VoucherDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zm.hashcode.hashpay.repository.jpa;
+
+import zm.hashcode.hashpay.model.vouchers.Voucher;
+import zm.hashcode.hashpay.repository.DAO;
+
+/**
+ *
+ * @author Bongani
+ */
+public interface VoucherDAO extends DAO<Voucher,Long>{
+    
+}
