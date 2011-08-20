fd16a7cf00dada4b06b6c18b0854e611b97aea9a
bonganim911@gmail.com <bonganim911@gmail.com@f78314b2-38be-a658-9164-56e6b81b6566>
2011-08-20 13:13:48 +0000

    Added voucherStatusType, claimType,CurrencyType enumerations in the model voucher.

---
+++ hashpay/src/java/zm/hashcode/hashpay/model/vouchers/ClaimTypeEnum.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zm.hashcode.hashpay.model.vouchers;
+
+/**
+ *
+ * @author Bongani
+ */
+public enum ClaimTypeEnum {
+    CUSTOMER_CLAIM,
+    ADMIN_CLAIM
+    
+}
---
+++ hashpay/src/java/zm/hashcode/hashpay/model/vouchers/VoucherStatusTypeEnum.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zm.hashcode.hashpay.model.vouchers;
+
+/**
+ *
+ * @author Bongani
+ */
+public enum VoucherStatusTypeEnum {
+    SOLD,
+    CLAIMED,
+    INVENTORY,
+    INVALID,
+    RETIRED
+    
+}
